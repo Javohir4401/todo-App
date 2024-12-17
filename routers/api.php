2afc6d37e95e3865980136348bd159cf50a7ca13
_m1ster_uzbek_ <jakbarov4401@gmail.com>
2024-12-17 14:37:50 +0500

    Update api.php

--- routers/api.php
+++ routers/api.php
@@ -1,16 +1,30 @@
 <?php
 
+use App\Router;
 use App\Todo;
 
-use App\Router;
-
 $router = new Router();
-
 $todo = new Todo();
 
-$router->get('/api/todos', function () use ($todo){
-   apiResponse($todo->getAllTodos(2));
-});
-$router->get('/api/todos/{id}', function ($todoId) use ($todo){
+$router->get('/api/todos', function () use ($todo) {
     apiResponse($todo->getAllTodos(2));
+});
+
+$router->get('/api/todos/{id}', function ($todoId) use ($todo) {
+    apiResponse($todo->getTodo($todoId));
+});
+
+$router->put('/api/update/{id}', function ($todoId) use ($todo) {
+    $todo->update($todoId, $_POST['title'], $_POST['status'], $_POST['dueDate']);
+    apiResponse([
+        'status' => 'OKEY',
+        'message' => 'UPDATE FUNCTION SUCCESSFULLY'
+        ]);
+});
+
+$router->get('/api/delete/{id}', function ($todoId) use ($todo) {
+    $todo->destroy($todoId);
+    header('Content-Type: application/json');
+    $response = array('status' => 'OKEY', 'message' => 'DELETE FUNCTION SUCCESSFULLY');
+    echo json_encode($response);
 });
